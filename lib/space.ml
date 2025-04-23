729b1557abb682e068f7d70ec9099a05cca6d28f
AngelVI <a.v.iliev13@gmail.com>
2025-04-23 10:26:36 +0300

    wip: space convertion

--- lib/space.ml
+++ lib/space.ml
@@ -1,5 +1,14 @@
 open Core
 
+type skey = SpaceKey of string
+
+let make_floor_str floor =
+  let postfix =
+    match abs floor with 1 -> "st" | 2 -> "nd" | 3 -> "rd" | _ -> "th"
+  in
+  sprintf "%d%s floor" floor postfix
+
+(*TODO: should this be a separate struct or just part of `t`*)
 type reservedProps = {
   reserved : bool;
   autoRelease : bool;
@@ -7,6 +16,8 @@ type reservedProps = {
   reservedById : string;
   reservedTime : Core.Time_ns.t;
 }
+(*[@@deriving show]*)
+(*how to derive show for Time_ns.t*)
 
 module Space = struct
   type t = {
@@ -16,7 +27,7 @@ module Space = struct
     resProps : reservedProps;
   }
 
-  let make number floor description =
+  let make ~number ~floor ~description =
     {
       number;
       floor;
@@ -34,32 +45,28 @@ module Space = struct
   let props_txt s =
     let desc = match s.description with "" -> "" | x -> sprintf " - %s" x in
     sprintf "(%d floor%s)" s.floor desc
+
+  let status_emoji s =
+    if s.reserved then ":large_orange_circle:" else ":large_green_circle:"
+
+  (** returns empty string if space is free, otherwise returns who reserved it*)
+  let status_description s =
+    if s.reserved then sprintf "<@%s>" s.reservedById else ""
+
+  let key s = SpaceKey (sprintf "%s %d" (make_floor_str s.floor) s.number)
+
+  let is_smaller s other =
+    match s with
+    | _ when s.floor < other.floor -> true
+    | _ when s.floor > other.floor -> false
+    | _ when s.floor = other.floor -> s.number < other.number
+    (*| _ -> failwith (sprintf "failed to compare %s with %s" s other)*)
+    (*| _ -> failwith (sprintf "failed to compare %s with %s" (key s) (key other))*)
+    | _ -> failwith (sprintf "failed to compare spaces")
+  (*TODO: fix this to print proper space info*)
 end
 
 (*TODO: Remaining convertions*)
-(*func (p *Space) GetStatusEmoji() string {*)
-(*	emoji := ":large_green_circle:"*)
-(*	if p.Reserved {*)
-(*		emoji = ":large_orange_circle:"*)
-(*	}*)
-(*	return emoji*)
-(*}*)
-(**)
-(*// GetStatusDescription Get space status description i.e. reserved, by who, when, etc.*)
-(*// Returns empty string if space is free*)
-(*func (p *Space) GetStatusDescription() string {*)
-(*	status := ""*)
-(*	if p.Reserved {*)
-(*		// timeStr := p.ReservedTime.Format("Mon 15:04")*)
-(*		// status = fmt.Sprintf("_:bust_in_silhouette:*<@%s>*\ton\t:clock1: *%s*_", p.ReservedById, timeStr)*)
-(*		status = fmt.Sprintf("<@%s>", p.ReservedById)*)
-(*	}*)
-(*	return status*)
-(*}*)
-(**)
-(*func (p *Space) Key() SpaceKey {*)
-(*	return MakeSpaceKey(p.Number, p.Floor)*)
-(*}*)
 (**)
 (*func (p *Space) Smaller(other *Space) bool {*)
 (*	if p.Floor < other.Floor {*)
@@ -71,22 +78,9 @@ end
 (*	}*)
 (*}*)
 (**)
-(*func MakeFloorStr(floor int) string {*)
-(*	postfix := "th"*)
 (**)
-(*	postfixes := []string{"st", "nd", "rd"}*)
-(*	absFloor := abs(floor)*)
-(*	if 1 <= absFloor && absFloor <= 3 {*)
-(*		postfix = postfixes[absFloor-1]*)
-(*	}*)
-(*	return fmt.Sprintf("%d%s floor", floor, postfix)*)
-(*}*)
-(**)
-(*func MakeSpaceKey(number, floor int) SpaceKey {*)
-(*	return SpaceKey(fmt.Sprintf("%s %d", MakeFloorStr(floor), number))*)
-(*}*)
 
 let%expect_test "props_txt" =
-  let s = Space.make 1 4 "Verification Room" in
+  let s = Space.make ~number:1 ~floor:4 ~description:"Verification Room" in
   printf "%s" (Space.props_txt s);
   [%expect {| (4 floor - Verification Room) |}]
