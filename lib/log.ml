fed19e5a44ec85b40a7f25f4971aff0f07837b24
AngelVI <a.v.iliev13@gmail.com>
2025-04-25 16:21:33 +0300

    formatter hell

--- lib/log.ml
+++ lib/log.ml
@@ -17,3 +17,21 @@ module Log = struct
   let warn = _log Warning
   let error = _log Error
 end
+
+let shout fmt = Printf.ksprintf (fun s -> s ^ "!") fmt
+let logv fmt = printf "%s" (Printf.ksprintf (fun s -> s ^ "!") fmt)
+let my_printf = Format.kfprintf (fun _ -> ()) Format.std_formatter
+
+let%expect_test "log.debug" =
+  Log.debug "hello world";
+  [%expect {| 2025-04-25 13:15:13.773326572Z: Debug: hello world |}]
+
+let%expect_test "log.debug with format" =
+  let x = "world" in
+  let res = shout "hello %s" x in
+  printf "%s" res;
+  [%expect {| hello world! |}]
+
+let%expect_test "gptk with format" =
+  my_printf "Number: %d, Word: %s\n" 42 "hello";
+  [%expect {| Number: 42, Word: hello |}]
