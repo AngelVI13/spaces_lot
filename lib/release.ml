3643234b53a2278d9a040832ba777c39595feae2
AngelVI <a.v.iliev13@gmail.com>
2025-05-03 10:32:29 +0300

    wip: release info functions

--- lib/release.ml
+++ lib/release.ml
@@ -70,7 +70,11 @@ module ReleaseInfo = struct
       `owner_id`, `owner_name` and `space_key` are required during release
       creation so they are not checked here *)
   let is_data_present r =
-    Option.is_some r.start_date && Option.is_some r.end_date
+    String.(r.releaser_id <> "")
+    && String.(r.owner_id <> "")
+    && String.(r.owner_name <> "")
+    && Option.is_some r.start_date
+    && Option.is_some r.end_date
 
   (** Checks if release dates are present and valid i.e. today <= start_date <=
       end_date *)
@@ -90,41 +94,87 @@ module ReleaseInfo = struct
         | _ -> assert false
       in
       Utils.is_date_range_valid ~today ~start_date ~end_date
+
+  let show_custom r =
+    (*NOTE: this uses https://github.com/janestreet/ppx_sexp_message*)
+    [%message
+      "ReleaseInfo"
+        ~space:(r.space_key : Space_key.t)
+        ~userName:(r.owner_name : string)
+        (r.start_date : Date.t option)
+        (r.end_date : Date.t option)
+        ~id:(r.unique_id : int option)]
+    |> Sexp.to_string_hum
+
+  (**)
+  (*func (i ReleaseInfo) DateRange() string {*)
+  (*	startDateStr := "nil"*)
+  (*	if i.StartDate != nil {*)
+  (*		startDateStr = i.StartDate.Format("2006-01-02")*)
+  (*	}*)
+  (**)
+  (*	endDateStr := "nil"*)
+  (*	if i.EndDate != nil {*)
+  (*		endDateStr = i.EndDate.Format("2006-01-02")*)
+  (*	}*)
+  (*	return fmt.Sprintf("%s -> %s", startDateStr, endDateStr)*)
+  (*}*)
 end
 
-(*TODO: finish this -> test the release.is_valid function*)
-(*let%expect_test "release.is_valid" =*)
+let print_is_valid_result r =
+  let res =
+    match ReleaseInfo.is_valid r with
+    | Ok _ -> sprintf "Release is valid"
+    | Error e -> sprintf "Release is not valid: %s" (Error.to_string_hum e)
+  in
+  print_string res
 
-(*  ReleaseInfo.make ~root_view_id:None ~releaser_id:"RELEASER_ID" ~owner_id:"OWNER_ID" ~owner_name:"OWNER_NAME"*)
-(*func (i ReleaseInfo) String() string {*)
-(*	startDateStr := "nil"*)
-(*	if i.StartDate != nil {*)
-(*		startDateStr = i.StartDate.Format("2006-01-02")*)
-(*	}*)
-(**)
-(*	endDateStr := "nil"*)
-(*	if i.EndDate != nil {*)
-(*		endDateStr = i.EndDate.Format("2006-01-02")*)
-(*	}*)
-(*	return fmt.Sprintf *)
-(*		"ReleaseInfo(space=%s, userName=%s, start=%s, end=%s, id=%d)",*)
-(*		i.Space.Key(),*)
-(*		i.OwnerName,*)
-(*		startDateStr,*)
-(*		endDateStr,*)
-(*		i.UniqueId,*)
-(*	)*)
-(*}*)
-(**)
-(*func (i ReleaseInfo) DateRange() string {*)
-(*	startDateStr := "nil"*)
-(*	if i.StartDate != nil {*)
-(*		startDateStr = i.StartDate.Format("2006-01-02")*)
-(*	}*)
-(**)
-(*	endDateStr := "nil"*)
-(*	if i.EndDate != nil {*)
-(*		endDateStr = i.EndDate.Format("2006-01-02")*)
-(*	}*)
-(*	return fmt.Sprintf("%s -> %s", startDateStr, endDateStr)*)
-(*}*)
+let make_test_release =
+  let space_key = Space_key.make ~floor:(-2) ~number:120 in
+  ReleaseInfo.make ~root_view_id:None ~releaser_id:"RELEASER_ID"
+    ~owner_id:"OWNER_ID" ~owner_name:"OWNER_NAME" ~space_key
+
+let%expect_test "release.show_custom" =
+  let r = make_test_release in
+  printf "%s" (ReleaseInfo.show_custom r);
+  [%expect
+    {|
+    (ReleaseInfo (space "-2nd floor 120") (userName OWNER_NAME) (r.start_date ())
+     (r.end_date ()) (id ()))
+    |}]
+
+let%expect_test "release.is_valid.missing_start_end" =
+  let r = make_test_release in
+  print_is_valid_result r;
+  [%expect
+    {|
+    Release is not valid: ("Missing date information for temporary release of space"
+     (r.space_key "-2nd floor 120") (r.start_date ()) (r.end_date ()))
+    |}]
+
+let%expect_test "release.is_valid.dates_present_but_range_incorrect" =
+  let r = make_test_release in
+  let r =
+    {
+      r with
+      start_date = Some (Date.of_string "2025-04-27");
+      end_date = Some (Date.of_string "2025-04-26");
+    }
+  in
+  print_is_valid_result r;
+  [%expect
+    {| Release is not valid: ("Start date is in the past" (start_str 2025-04-27)) |}]
+
+(*TODO: this test is bad because the `ReleaseInfo.is_valid` is based on
+  today's date. Thats why I'm using dates in the far future to test this*)
+let%expect_test "release.is_valid" =
+  let r = make_test_release in
+  let r =
+    {
+      r with
+      start_date = Some (Date.of_string "2029-04-26");
+      end_date = Some (Date.of_string "2029-04-28");
+    }
+  in
+  print_is_valid_result r;
+  [%expect {| Release is valid |}]
